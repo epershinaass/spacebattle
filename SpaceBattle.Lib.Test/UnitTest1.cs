33ceaa041d20424e5dd3ea18f71362bb1ff11e29
Elizaveta Pershina <epershina.ass@gmail.com>
2022-10-15 12:09:37 +0600

    initial commit

---
+++ SpaceBattle.Lib.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SpaceBattle.Lib.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ SpaceBattle/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SpaceBattle
+{
+    public class Class1
+    {
+
+    }
+}

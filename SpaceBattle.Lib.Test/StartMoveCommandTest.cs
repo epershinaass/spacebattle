a5b549dd150a3525b60eb306b5d2aced5e1c9fcd
Elizaveta Pershina <epershina.ass@gmail.com>
2022-12-30 09:25:22 +0600

    99 coverage

--- SpaceBattle.Lib.Test/StartMoveCommandTest.cs
+++ SpaceBattle.Lib.Test/StartMoveCommandTest.cs
@@ -6,8 +6,8 @@ namespace SpaceBattle.Lib.Test;
 
 public class StartMoveCommandTest 
 {
-    [Fact]
-    public void TestStartMoveCommand()
+        
+    public StartMoveCommandTest()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set",  IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
--- SpaceBattle/IMoveCommandStartable.cs
+++ SpaceBattle/IMoveCommandStartable.cs
@@ -2,7 +2,6 @@ namespace SpaceBattle
 {
     public interface IMoveCommandStartable
     {
-
         IUObject Obj { get; }
         Vector InitialVelocity { get; }
     }

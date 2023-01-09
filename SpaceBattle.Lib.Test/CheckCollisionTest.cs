ffbc072f55ba9742d5458b2a04b176f29cfe262f
Elizaveta Pershina <epershina.ass@gmail.com>
2023-01-09 13:30:46 +0600

    tests again

--- SpaceBattle.Lib.Test/CheckCollisionTest.cs
+++ SpaceBattle.Lib.Test/CheckCollisionTest.cs
@@ -7,8 +7,8 @@ namespace SpaceBattle.Lib.Test;
 public class TestChekCollision
 {
     Mock<IStrategy> getDecisionTreeStrategy = new Mock<IStrategy>();
-    GetDifferenceStrategy getDifferenceStrategy = new GetDifferenceStrategy();
-    GetPropertyStrategy getPropertyStrategy = new GetPropertyStrategy();
+    StrategyGetDifference getDifferenceStrategy = new StrategyGetDifference();
+    StrategyGetProperty getPropertyStrategy = new StrategyGetProperty();
     public TestChekCollision()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
@@ -18,4 +18,24 @@ public class TestChekCollision
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceBattle.GetDifference", (object[] args) => getDifferenceStrategy.ExecuteStrategy(args)).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceBattle.GetDecisionTree", (object[] args) => getDecisionTreeStrategy.Object.ExecuteStrategy(args)).Execute();
     }
+
+    [Fact]
+    public void CollisionsFoundTest()
+    {
+        var obj1 = new Mock<IUObject>();
+        var obj2 = new Mock<IUObject>();
+
+        obj1.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(It.IsAny<int>(), It.IsAny<int>()));
+        obj1.Setup(x => x.GetProperty("Position")).Returns(new Vector(It.IsAny<int>(), It.IsAny<int>()));
+        obj2.Setup(x => x.GetProperty("Velocity")).Returns(new Vector(It.IsAny<int>(), It.IsAny<int>()));
+        obj2.Setup(x => x.GetProperty("Position")).Returns(new Vector(It.IsAny<int>(), It.IsAny<int>()));
+
+        getDecisionTreeStrategy.Setup(c => c.ExecuteStrategy(It.IsAny<object[]>())).Returns(true).Verifiable();
+        CheckCollision collision = new CheckCollision(obj1.Object, obj2.Object);
+
+        Assert.Throws<Exception>(() => collision.Execute());
+        getDecisionTreeStrategy.Verify();
+    }
+
 }
+
